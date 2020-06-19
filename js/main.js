e07576dd6a0f7908035bf2b115c33de532183458
CrazyCarrot <crazymorkovko@gmail.com>
2020-06-19 14:53:41 -0700

    fix mistakes

--- js/main.js
+++ js/main.js
@@ -2,7 +2,6 @@ $(document).ready(function () {
   const mMenuButton = $(".m-menu-button");
   const mMenu = $(".m-menu");
   const tab = $(".tab");
-  const mMenuImage = $(".m-menu-image");
   var mySwiper = new Swiper(".swiper-container", {
     spaceBetween: 25,
     slidesPerView: 4,
@@ -27,7 +26,6 @@ $(document).ready(function () {
   mMenuButton.on("click", function () {
     mMenu.toggleClass("active");
     $("body").toggleClass("no-scroll");
-    mMenuImage.toggleClass("m-menu-active");
   });
 
   tab.on("click", function () {
